0356a440d800800deecaf904d490d7c33e2b1a54
Pingu2 <wittenborn2@yahoo.com>
2024-10-21 18:14:38 -0400

    Add basic drive code

--- src/main.cpp
+++ src/main.cpp
@@ -1,28 +1,31 @@
 #include "vex.h"
 #include "robotConfig.h"
+#include "drivetrain.h"
 
-// A global instance of competition
-vex::competition competition;
+Drivetrain drive(config::leftBaseMotors, config::rightBaseMotors, 3.25, 13.75, 36.0 / 48.0);
 
-void autonomous(void) {
-  
-}
+void autonomous() {}
 
-void usercontrol(void) {
-  while (true) {
-    vex::wait(20, vex::msec); // Prevent hogging resources
-  }
+void userControl() {
+    while (true) {
+        drive.moveCurvatureVoltage(
+            config::controller.Axis3.position(), // Forward
+            config::controller.Axis4.position()  // Turn
+        );
+
+        vex::wait(20, vex::msec); // Prevent hogging resources
+    }
 }
 
 int main() {
-  // Set up callbacks for autonomous and driver control periods.
-  competition.autonomous(autonomous);
-  competition.drivercontrol(usercontrol);
+    // Set up callbacks for autonomous and driver control periods.
+    config::competition.autonomous(autonomous);
+    config::competition.drivercontrol(userControl);
 
-  // Initialize things
+    // Initialize things
 
-  // Prevent main from exiting with an infinite loop.
-  while (true) {
-    vex::wait(100, vex::msec);
-  }
+    // Prevent main from exiting with an infinite loop.
+    while (true) {
+        vex::wait(100, vex::msec);
+    }
 }
