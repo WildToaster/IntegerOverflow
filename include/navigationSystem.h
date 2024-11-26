8beea92924775e3e06245a0d89639ca09b45f405
Pingu2 <wittenborn2@yahoo.com>
2024-11-25 19:01:39 -0500

    Create Odometry Loop

--- include/navigationSystem.h
+++ include/navigationSystem.h
@@ -1,19 +1,31 @@
 #pragma once
 
 #include "vex.h"
+#include "drivetrain.h"
 
 struct Location {
-    float x, y, heading;
+    float x = 0;
+    float y = 0;
+    float heading = 0;
 
     Location(float x, float y, float heading): x(x), y(y), heading(heading) {};
 };
 
 class NavigationSystem {
     public:
-    NavigationSystem(vex::gps& gpsSensor);
+    NavigationSystem(vex::gps& gpsSensor, Drivetrain& drive);
     Location getLocation();
 
     private:
     Location getGPSPacket();
+    static void odomLoop(void* thisArg);
+    void odomStep();
+
     vex::gps& gpsSensor;
+
+    vex::thread odomThread;
+    Drivetrain& drive;
+
+    Location lastPosition();
+    Location basePosition();
 };
--- src/navigationSystem.cpp
+++ src/navigationSystem.cpp
@@ -1,6 +1,15 @@
 #include "navigationSystem.h"
 
-NavigationSystem::NavigationSystem(vex::gps& gps): gpsSensor(gps) {};
+void NavigationSystem::odomLoop(void* thisArg) {
+    while (true) {
+        ((NavigationSystem*)thisArg)->odomStep();
+    }
+}
+
+NavigationSystem::NavigationSystem(vex::gps& gps, Drivetrain& drive): gpsSensor(gps), drive(drive) {
+    odomThread = vex::thread(odomLoop, this);
+    odomThread.detach();
+};
 
 Location NavigationSystem::getGPSPacket() {
     return Location(
@@ -11,4 +20,10 @@ Location NavigationSystem::getGPSPacket() {
 
 Location NavigationSystem::getLocation() {
     return getGPSPacket();
+}
+
+void NavigationSystem::odomStep() {
+    
+
+    vex::this_thread::sleep_for(10);
 }
