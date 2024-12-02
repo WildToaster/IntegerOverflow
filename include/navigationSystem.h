bea7a51ff900a28470958f8e2bf06aef1a80171b
Pingu2 <wittenborn2@yahoo.com>
2024-12-01 19:28:04 -0500

    Add getAverageLocation to navigationSystem

--- include/navigationSystem.h
+++ include/navigationSystem.h
@@ -8,9 +8,11 @@ struct Location {
     float y = 0;
     float heading = 0;
 
+    Location() {};
     Location(float x, float y, float heading): x(x), y(y), heading(heading) {};
 };
 
 extern Location getLocation();
+extern Location getAverageLocation(int iterations = 10);
 
 }
--- src/navigationSystem.cpp
+++ src/navigationSystem.cpp
@@ -14,4 +14,22 @@ Location getLocation() {
     return getGPSPacket();
 }
 
+Location getAverageLocation(int iterations) {
+    Location locationSum;
+
+    for (int i = 0; i < iterations; i++) {
+        Location currentLocation = getLocation();
+        locationSum.x += currentLocation.x;
+        locationSum.y += currentLocation.y;
+        locationSum.heading += currentLocation.heading;
+
+        vex::this_thread::sleep_for(20);
+    }
+
+    locationSum.x /= iterations;
+    locationSum.y /= iterations;
+    locationSum.heading /= iterations;
+    return locationSum;
+}
+
 }
