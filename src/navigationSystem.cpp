4033212a5c4a98cea1d65b507ae5f0a466c0ee80
Pingu2 <wittenborn2@yahoo.com>
2025-02-03 18:29:37 -0500

    Add first implementation of odometry

--- src/navigationSystem.cpp
+++ src/navigationSystem.cpp
@@ -1,7 +1,81 @@
 #include "navigationSystem.h"
 #include "robotConfig.h"
+#include <cmath>
 
 namespace nav {
+
+float odomX = 0;
+float odomY = 0;
+float odomHeading = 0;
+
+float baselineOdomX = 0;
+float baselineOdomY = 0;
+float baselineOdomHeading = 0;
+
+float getAverageLeftOdomWheelPosition() {
+    return (config::leftFrontBase.position(vex::rotationUnits::deg) +
+        config::leftMiddleBase.position(vex::rotationUnits::deg) + 
+        config::leftBackBase.position(vex::rotationUnits::deg)) / 3;
+}
+
+float getAverageRightOdomWheelPosition() {
+    return (config::rightFrontBase.position(vex::rotationUnits::deg) +
+        config::rightMiddleBase.position(vex::rotationUnits::deg) + 
+        config::rightBackBase.position(vex::rotationUnits::deg)) / 3;
+}
+
+// See https://wiki.purduesigbots.com/software/odometry
+void odometryLoop() {
+    const float wheelCircumference = 3.25 * M_PI;
+    const float gearing = 48.0 / 36.0; // Wheel / motor
+    const float wheelbase = 13.75;
+
+    float lastLeftDist = 0;
+    float lastRightDist = 0;
+    float lastHeading = 0;
+
+    while (true) {
+        // Get current encoder values
+        float leftEncoder = getAverageLeftOdomWheelPosition();
+        float rightEncoder = getAverageRightOdomWheelPosition();
+
+        // Calculate distance traveled by left and right wheels
+        float totalLeftDist = leftEncoder * gearing / 360 * wheelCircumference;
+        float totalRightDist = rightEncoder * gearing / 360 * wheelCircumference;
+
+        float deltaLeftDist = totalLeftDist - lastLeftDist;
+        float deltaRightDist = totalRightDist - lastRightDist;
+
+        // Update previous encoder values
+        lastLeftDist = totalLeftDist;
+        lastRightDist = totalRightDist;
+        lastHeading = odomHeading;
+
+        // Calculate new orientation in radians
+        float totalHeading = baselineOdomHeading + (totalLeftDist - totalRightDist) / wheelbase;
+
+        // Calculate change in orientation
+        float deltaHeading = totalHeading - lastHeading;
+
+        float localOffset;
+        if (deltaHeading == 0) {
+            localOffset = deltaRightDist;
+        } else {
+            localOffset = 2 * std::sin(deltaHeading * 0.5) * (deltaRightDist / deltaHeading + wheelbase * 0.5);
+        }
+
+        // Transform localOffset into the global frame
+        float offsetAngle = lastHeading + deltaHeading / 2;
+
+        float globalOffsetX = -localOffset * std::sin(-offsetAngle);
+        float globalOffsetY =  localOffset * std::cos(-offsetAngle);
+
+        odomX += globalOffsetX;
+        odomY += globalOffsetY;
+
+        vex::this_thread::sleep_for(5);
+    }
+}
 
 Location getGPSPacket() {
     return Location(
